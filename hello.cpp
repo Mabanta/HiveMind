b90b58cfc1c008d89bf054dc1b8f664d137f7b04
Matthew Lynch <lynch.matt.m@gmail.com>
2022-02-02 17:36:17 -0500

    Added factorial() to hello.cpp
    factorial() takes an integer and returns an integer that is the factorial of passed value.

--- hello.cpp
+++ hello.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
 
+// A more useful function
+int factorial(int);
+
 int main()
 {
     std::cout << "Hello C++ world from team HiveMind!" << std::endl;
+    std::cout << "5! = " << factorial(5) << std::endl;
     return 0;
+}
+
+int factorial(int a)
+{
+    int returnVal = 1;
+    while(a > 1)
+    {
+        returnVal *= a--;
+    }
+    return returnVal;
 }
