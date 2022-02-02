8c4be6a0ac9d5daf9fae20f28b52035d2d6f3d57
Matthew Lynch <lynch.matt.m@gmail.com>
2022-02-02 17:32:51 -0500

    Added useless add() function to hello.cpp
    It is actually just a waste of space...

--- hello.cpp
+++ hello.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
 
+// Really stupid function (just use +)
+int add(int, int);
+
 int main()
 {
     std::cout << "Hello C++ world from team HiveMind!" << std::endl;
+
+    int dummyVar = 0;
+    std::cout << "Running add() on dummyVar: " <<  add(dummyVar++, ++dummyVar) << std::endl;
+    std::cout << "dummyVar is now: " << dummyVar << std::endl;
+
     return 0;
+}
+
+int add(int a, int b)
+{
+    return a + b;
 }
