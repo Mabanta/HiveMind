c3315eec48298dfbedffed0fa518f28573623ffc
etaeckens <etaeckens@gmail.com>
2022-08-31 15:40:39 -0400

    Updated cluster_tracking_2
    Cluster_tracking_2 can now accurately track the bees.

--- Cluster_tracking_2.py
+++ Cluster_tracking_2.py
@@ -5,16 +5,21 @@ import math
 
 math_e = math.e
 
-alpha = 0.02 #This factor controls how sensitive a cluster is to location change based on new spikes
+alpha = 0.15 #This factor controls how sensitive a cluster is to location change based on new spikes
 #A higher value will cause the cluster to adapt more quickly, but it will also move more sporadically
 
-SCALE_FACTOR = 0.6 #A scale factor of 0 means no accumulation
+SCALE_FACTOR = 0.995 #A scale factor of 0 means no accumulation
+IMG_SCALE_FACTOR = 0.4
 #Scale factors close to 1 mean accumulation for a long time
 
-frame_rate = 500;
-DELAY_TIME = int(1000000/frame_rate)
+frame_rate = 100;
+DISPLAY_TIME = int(1000000/frame_rate)
 #Frame rate is used to control the display
 #The actual algorithm won't use frames, but we have to use frames if we want to see the data
+
+update_rate = 150;
+DELAY_TIME = int(1000000/update_rate)
+#This controls how often certain costly procedures are performed, such as checking for new clusters
 
 start_time = 0
 FIRST_EVENT = 200000
@@ -24,26 +29,37 @@ BLUR_SCALE = 20
 #This algorithm uses a "blur" to make it easier to detect a lot of events occurring in the same region
 #The algorithm breaks the time surface into 20 x 20 regions and keeps track of how many events have occurred in each region
 #The blur scale controls the size of each region
-increase_factor_blurred = 0.01
+increase_factor_blurred = 0.18
 #This controls how much each event contributes to the regions in the blurred time surface
 #A higher number will make each region more sensitive to individual events
 
 
-max_clusters = 6
-#This puts a limit on how many clusters can be formed
-cluster_init_threshold = 0.8
-#This is the value that a region in the blurred time surface must reach in order to initiate a cluster
-cluster_sustain_threshold = 5
-#This is the number of events that must occur within a certain time inside a cluster in order for it to survive
-cluster_sustain_time = DELAY_TIME*20
-#This is the amount of time that the program waits before checking if a cluster needs to be removed
+max_clusters = 15 #This puts a limit on how many clusters can be formed
+cluster_init_threshold = 0.9 #This is the value that a region in the blurred time surface must reach in order to initiate a cluster
+cluster_sustain_threshold = 18 #This is the number of events that must occur within a certain time inside a cluster in order for it to survive
+cluster_sustain_time = 40000 #This is the amount of time that the program waits before checking if a cluster needs to be removed
+
+
+
+total_clusters = 0
+
+radius_growth_factor = 1.0008
+radius_shrink_factor = 0.998
+
+colors = [(0,0,255),(0,255,0),(255,0,0),(255,255,0),(255,0,255)]
+color_index = 0
 
 class Cluster():
 
-    def __init__(self,x,y):
+    def __init__(self,x,y,color):
         self.x = x
         self.y = y
-        self.radius = 50
+        self.radius = 25.0
+        self.vel_x = 0
+        self.vel_y = 0
+        self.prev_x = x
+        self.prev_y = y
+        self.color = color
         #This controls how large the clusters are
         self.count = 0
 
@@ -57,16 +73,27 @@ class Cluster():
         return False
     #This determines whether an event happened inside the cluster
 
+    def borderRange(self,x,y):
+        if self.distance(x,y) < self.radius * 1.33:
+            return True
+        return False
+
+    def otherClusterRange(self,x,y):
+        if self.distance(x,y) < self.radius * 2:
+            return True
+        return False
+
     def shift(self,x,y):
         self.x = (1-alpha)*self.x + alpha*x
         self.y = (1-alpha)*self.y + alpha*y
     #This shifts the location of the cluster based on events that happen inside it
 
 def sigmoid(x):
-    return (math_e**x-math_e**(-x))/(math_e**x+math_e**(-x))
+    #return (math_e**x-math_e**(-x))/(math_e**x+math_e**(-x))
+    return x / (1 + np.abs(x))
     #This function limits each pixel in a time surface to a max value of 1
 
-with AedatFile('tossing_multiple-2022_03_14_16_31_50.aedat4') as f:
+with AedatFile('summer_bees_video_2022_08_13.aedat4') as f:
     # list all the names of streams in the file
     print(f.names)
 
@@ -80,6 +107,7 @@ with AedatFile('tossing_multiple-2022_03_14_16_31_50.aedat4') as f:
     #Initializes the blurred time surface, used to control the creation of new clusters
 
     next_time = DELAY_TIME
+    next_frame = DISPLAY_TIME
     next_sustain_time = cluster_sustain_time
 
     clusters = []
@@ -88,79 +116,102 @@ with AedatFile('tossing_multiple-2022_03_14_16_31_50.aedat4') as f:
     #Puts the event data into an indexable array
     
     start_time = events[0]['timestamp'] #Sets the start time to the time that first event occurs
+    prev_time = start_time
 
     for i in range(FIRST_EVENT,len(events)):
         e = events[i]
         
         event_time = e['timestamp'] - start_time #Assigns a time to the event based off of the initial time
 
-        ts[int(e['y']),int(e['x'])][:] = 1 #Updates the time surface - this is purely for visualization purposes at this point
-        ts_blurred[int(e['y']/BLUR_SCALE),int(e['x']/BLUR_SCALE)] += increase_factor_blurred
-        #Increases the value of the corresponding region in the blurred time surface
+        if not e['polarity']:
+            ts[int(e['y']),int(e['x'])][:] = 1 #Updates the time surface - this is purely for visualization purposes at this point
+            ts_blurred[int(e['y']/BLUR_SCALE),int(e['x']/BLUR_SCALE)] += increase_factor_blurred
+            #Increases the value of the corresponding region in the blurred time surface
 
-        distances = [] #Finds the distance of the event from each existing cluster
-        for cluster in clusters:
-            distances.append(cluster.distance(int(e['x']),int(e['y'])))
+            distances = [] #Finds the distance of the event from each existing cluster
+            for cluster in clusters:
+                distances.append(cluster.distance(int(e['x']),int(e['y'])))
+                cluster.x += cluster.vel_x * (event_time - prev_time)
+                cluster.y += cluster.vel_y * (event_time - prev_time)
 
-        if len(clusters) > 0:
-            min_cluster = clusters[distances.index(min(distances))] #Finds the closest cluster to the event
-            if min_cluster.inRange(int(e['x']),int(e['y'])): #If the event is inside the closest cluster, it updates the location of that cluster
-                min_cluster.shift(int(e['x']),int(e['y']))
-                min_cluster.count += 1 #Used to track the number of events that have occurred in the cluster
+            if len(clusters) > 0:
+                min_cluster = clusters[distances.index(min(distances))] #Finds the closest cluster to the event
+                if min_cluster.inRange(int(e['x']),int(e['y'])): #If the event is inside the closest cluster, it updates the location of that cluster
+                    min_cluster.shift(int(e['x']),int(e['y']))
+                    min_cluster.count += 1 #Used to track the number of events that have occurred in the cluster
+
+                elif min_cluster.borderRange(int(e['x']),int(e['y'])):
+                    min_cluster.radius *= radius_growth_factor
+
+            prev_time = event_time
+            
+        ts_blurred = ts_blurred*SCALE_FACTOR
+        ts_blurred[ts_blurred > 1] = 1
         
-        if (event_time > next_time): #Updates the display after one frame has passed
+        if (event_time > next_time): #Checks for new clusters (costly computation, can't be performed every event)
+            next_time += DELAY_TIME;
 
             for x in range(int(width/BLUR_SCALE)):
                 for y in range(int(height/BLUR_SCALE)):
-                    ts_blurred[y,x] = sigmoid(ts_blurred[y,x]) #Makes sure each region in the blurred time surface has a max value of 1
-
                     
                     if (ts_blurred[y,x] > cluster_init_threshold) and (len(clusters) < max_clusters): 
                         alreadyAdded = False
                         
                         for cluster in clusters: 
-                            if cluster.inRange(x*BLUR_SCALE,y*BLUR_SCALE):
+                            if cluster.otherClusterRange(x*BLUR_SCALE,y*BLUR_SCALE):
                                 alreadyAdded = True
 
                         if not alreadyAdded:
-                            new_cluster = Cluster(x*BLUR_SCALE,y*BLUR_SCALE)
+                            new_cluster = Cluster(x*BLUR_SCALE,y*BLUR_SCALE,colors[color_index])
                             clusters.append(new_cluster)
+                            total_clusters += 1
+                            
+                            color_index += 1
+                            if (color_index > 4):
+                                color_index = 0
+                            #print(f"Total Clusters: {total_clusters}")
                             #Adds a new cluster if three criteria are met:
                             #The region must be greater than the cluster initialization threshold
                             #The region can't be inside an already existing cluster
                             #There can't be more clusters than the max limit
-                    
 
-            
-            next_time += DELAY_TIME;
-            
-            track_img = ts.copy()
-            img_blurred = ts_blurred.copy()
+            for cluster in clusters: #Updates velocity and size of each cluster
+                cluster.vel_x = (cluster.x - cluster.prev_x) / DELAY_TIME
+                cluster.vel_y = (cluster.y - cluster.prev_y) / DELAY_TIME
+                cluster.prev_x = cluster.x
+                cluster.prev_y = cluster.y
 
-            for cluster in clusters: #Displays each of the clusters
-                cv2.circle(track_img,(int(cluster.x),int(cluster.y)),cluster.radius,(0,0,255))
+                cluster.radius *= radius_shrink_factor
+
 
             if (event_time > next_sustain_time):
                 next_sustain_time += cluster_sustain_time
-
                 
                 for cluster in clusters: #Resets the event count of each cluster and removes clusters if they didn't reach the minimum number of events
-                    cluster.count = 0
                     if cluster.count < cluster_sustain_threshold:
                         clusters.remove(cluster)
+                    cluster.count = 0
+            
+
+        if (event_time > next_frame):
+            next_frame += DISPLAY_TIME
+            
+            track_img = ts.copy()
+            #img_blurred = ts_blurred.copy()
+            for cluster in clusters:
+                cv2.circle(track_img,(int(cluster.x),int(cluster.y)),int(cluster.radius),cluster.color)
+
                 
 
             
-            cv2.imshow('Time_Surface',cv2.resize(track_img,(int(width/2),int(height/2)))) #Displays the time surface to the screen
+            cv2.imshow('Time_Surface',cv2.resize(track_img,(int(width/1),int(height/1)))) #Displays the time surface to the screen
             cv2.waitKey(1)
 
-            #cv2.imshow('Blurred',cv2.resize(img_blurred,(int(width/2),int(height/2)))) #Displays the time surface to the screen
-            #cv2.waitKey(1)
+            '''cv2.imshow('Blurred',cv2.resize(img_blurred,(int(width/1),int(height/1)))) #Displays the time surface to the screen
+            cv2.waitKey(1)'''
+
+            ts = ts*IMG_SCALE_FACTOR
 
             
-            
-                    
-
-            ts = ts*SCALE_FACTOR
-            ts_blurred = ts_blurred*SCALE_FACTOR
+        
 
